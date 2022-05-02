4dea67fb3e6d046e239861f7cc6738374e166cb5
Daviichii89 <davichi89@gmail.com>
2022-05-03 00:16:17 +0200

    testing todolist app

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import React, {useState} from "react";
+import { nanoid } from 'nanoid';
+
+function App() {
+  const [tarea,setTarea] = useState('');
+  const [tareas,setTareas] = useState([]);
+  const [modoEdicion,setModoEdicion] = useState(false);
+  const [id,setId] = useState('');
+  const [error,setError] = useState(null);
+  const agregarTarea = e => {
+    e.preventDefault();
+    if(!tarea.trim()){
+      console.log("Elemento vacio");
+      setError("Escriba algo porfavor..."); 
+      return
+    }
+    console.log(tarea);
+    setTareas([
+      ...tareas,
+      {id: nanoid(10), nombreTarea: tarea}
+    ])
+    setTarea('');
+    setError('');
+  }
+  const eliminarTarea = id => {
+    //console.log(id);
+    const arrayFiltrado = tareas.filter(item => item.id !== id)
+    setTareas (arrayFiltrado)
+  }
+  const editar = item => {
+    setModoEdicion(true)
+    setTarea(item.nombreTarea)
+    setId(item.id)
+  }
+  const editarTarea = e => {
+    e.preventDefault()
+    if(!tarea.trim()){
+      console.log("Elemento vacio");
+      setError("Escriba algo porfavor...");
+      return;
+    }
+    const arrayEditado = tareas.map(item => item.id === id ? {id, nombreTarea: tarea} : item)
+    setTareas(arrayEditado);
+    setModoEdicion(false);
+    setTarea('');
+    setId('');
+    setError('');
+  }
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD Simple</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de tareas</h4>
+          <ul className="list-group">
+            {
+              tareas.length === 0 ? 
+                (
+                  <li className="list-group-item">No hay tareas</li>
+                ) 
+              : 
+                (  
+                  tareas.map(item => (
+                    <li className="list-group-item" key={item.id}>
+                      <span className="lead">{item.nombreTarea}</span>
+                      <button 
+                        className="btn btn-danger btn-sm float-right mx-2"
+                        onClick={() => eliminarTarea(item.id)}
+                      >
+                        Eliminar
+                      </button>
+                      <button 
+                        className="btn btn-warning btn-sm float-right"
+                        onClick={() => editar(item)}
+                      >
+                        Editar
+                      </button>
+                    </li>
+                  ))
+                )
+            }
+          </ul>
+        </div>
+        
+            <div className="col-4">
+            <h4 className="text-center">
+              {
+                modoEdicion ? "Editar tarea" : "Agregar tarea"
+              }
+            </h4>
+            <form data-testid="form" onSubmit={modoEdicion ? editarTarea : agregarTarea}>
+              {
+                error ? <span className="text-danger">{error}</span> : null
+              }
+              <input 
+                type="text" 
+                className="form-control mb-2"
+                placeholder="Ingrese tarea"
+                onChange={e => setTarea(e.target.value)}
+                value={tarea}
+              />
+              {
+                modoEdicion ? (
+                  <button className="btn btn-warning btn-block" type="submit">Editar</button>
+                ):
+                (
+                  <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+                )
+              }
+            </form>
+            </div>      
+      </div>      
+    </div>
+  );
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+    it('Render the App component', () => {
+      render(<App />)
+      
+      expect(screen.getByText('No hay tareas')).toBeInTheDocument();
+    })
+    it('Click the button Agregar', () => {
+      const mockHandler = jest.fn();
+      render(<App agregarTarea={mockHandler}/>);
+
+      /*
+      const addButton = screen.getByText('Agregar');
+      expect(addButton).toBeInTheDocument();
+      userEvent.click(addButton);
+      */
+      const submitForm = (screen.getByTestId("form"));
+      //expect(submitForm);
+      fireEvent.submit(submitForm)
+
+      // falta el mock para ejecutar la funcion del boton
+      expect(mockHandler.mock.calls).toHaveLength(1); 
+    })
 });
